264d3309749612afe7326dc883cabcdf9ef9a6d1
haopining <haopining@gmail.com>
2025-03-03 21:19:36 +0800

    Revert to commit c529d13b

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,6 @@ const config: NextConfig = {
   },
   images: {
     remotePatterns: [{hostname: 'cdn.sanity.io'}],
-    unoptimized: true, // 為了靜態導出，禁用圖片優化
   },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
@@ -28,12 +27,6 @@ const config: NextConfig = {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: 'false',
   },
-  // GitHub Pages 部署設置
-  output: 'export', // 啟用靜態導出
-  // 使用條件判斷，僅在生產環境設置 basePath
-  basePath: process.env.NODE_ENV === 'production' ? '/pinhaocrypto.github.io' : '',
-  // 確保資源正確加載
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pinhaocrypto.github.io' : '',
 }
 
 export default config
